0c778022ddf01cad91c96dccde1d9e179ac80fcc
Darlison <Darlison001@gmail.com>
2020-12-20 16:55:11 -0300

    add variaveis de ambient e confg em server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3333;
+const port = process.env.PORT_SERVER || 3333;
 
 app.listen(port, ()=> console.log(`Server Up in port ${port}`))
